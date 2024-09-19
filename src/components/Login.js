31411305c954e3484495401cc4c37db688f7117e
DESKTOP-SKVEC0C\Admin <saiteja.a@versatilemobitech.com>
2024-09-19 16:26:41 +0530

    adde text

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Login() {
     return (
         <div>
-            <h1>login page in react</h1>
+            <h1>login page in reac dadt</h1>
         </div>
     )
 }
